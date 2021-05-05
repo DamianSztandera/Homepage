6965ebc34b0fc3ac0f2d17d8f6bd781849421f0b
DamianSztandera <biuro@foto-image.pl>
2021-05-05 21:45:44 +0200

    create const in js

--- srpipt.js
+++ srpipt.js
@@ -1,14 +1,22 @@
-console.log("Miałem dzisiaj dobry dzień, nie spiernicz tego :D !!");
-let action = document.querySelector(".action");
-let table = document.querySelector(".table");
-let changeTableColor = document.querySelector(".changeTableColor");
+{
+  const Welcome = () => {
+    console.log("Miałem dzisiaj dobry dzień, nie spiernicz tego :D !!");
+    }
+    
+    const onChangeColorTableClick = () => {
+      table.classList.toggle("table__changeColor");
+      if (table.classList.contains("table__changeColor")) {
+        changeTableColor.innerText = "pierwszy";
+      } else {
+        changeTableColor.innerText = "drugi";
+      }
+    };
+    const action = document.querySelector(".action");
+    const table = document.querySelector(".table");
+    const changeTableColor = document.querySelector(".changeTableColor");
+    
+    action.addEventListener("click", onChangeColorTableClick);
 
-action.addEventListener("click", () => {
-  table.classList.toggle("table__changeColor");
-  if (table.classList.contains("table__changeColor")) {
-    changeTableColor.innerText = "pierwszy";
-  } else {
-    changeTableColor.innerText = "drugi";
-  }
-});
-
+    Welcome();
+    
+}
